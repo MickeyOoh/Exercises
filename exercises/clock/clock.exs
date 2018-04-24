05456ca54345317855b61580142bb4e41d499cb6
MickeyOoh <mikio.westvil@gmail.com>
2018-04-24 12:31:44 -0400

    mv change to implement

--- exercises/clock/clock.exs
+++ exercises/clock/clock.exs
@@ -1,5 +1,7 @@
 defmodule Clock do
   defstruct hour: 0, minute: 0
+  @type t :: %Clock{hour: integer, minute: integer}
+  @type t(hour, minute) :: %Clock{hour: hour, minute: minute}
 
   @doc """
   Returns a string representation of a clock:
@@ -7,8 +9,10 @@ defmodule Clock do
       iex> Clock.new(8, 9) |> to_string
       "08:09"
   """
-  @spec new(integer, integer) :: Clock
+  @spec new(integer, integer) :: Clock.t()
   def new(hour, minute) do
+    %Clock{} = %{hour: hour, minute: minute}
+    "#{hour}:#{minute}"
   end
 
   @doc """
@@ -19,5 +23,14 @@ defmodule Clock do
   """
   @spec add(Clock, integer) :: Clock
   def add(%Clock{hour: hour, minute: minute}, add_minute) do
+  end
+
+  defimpl String.Chars, for: Clock do
+    def to_string(%Clock{hour: hour, minute: minute}) do 
+      "#{format(hour)}:#{format(minute)}"
+    end
+    defp format(number) do 
+      number |> Integer.to_string() |> String.pad_leading(2, "0")
+    end
   end
 end
