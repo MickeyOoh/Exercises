dae94d58f392508f0d78e878a4cea6f273009758
MickeyOoh <mikio.westvil@gmail.com>
2018-07-04 11:03:23 -0400

    transpose finished

--- implements/transpose/transpose.exs
+++ implements/transpose/transpose.exs
@@ -18,5 +18,26 @@ defmodule Transpose do
 
   @spec transpose(String.t()) :: String.t()
   def transpose(input) do
+    rows = String.split(input, "\n")
+    rows
+    |> padding( )
+    |> Enum.map( &String.codepoints/1)
+    |> transpose_zip()
+    |> Enum.map(  &Enum.join/1) 
+    |> Enum.join( "\n")
+    |> String.trim_trailing() 
+  end
+  def transpose_zip(matrix) do 
+    matrix
+    |> List.zip()
+    |> Enum.map(&Tuple.to_list/1)
+  end
+  def get_longest_rows(rows) do 
+    Enum.map(rows, &String.length/1)
+    |> Enum.max()
+  end
+  def padding(rows) do 
+    len = get_longest_rows(rows)
+    Enum.map(rows, &(String.pad_trailing(&1,len)))
   end
 end
